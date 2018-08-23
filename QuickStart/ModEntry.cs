fe6660e1be46f6bb43eba0b988fb2d841c4ee16b
Brian-Wuest <Brian.Wuest@outlook.com>
2018-08-23 05:33:12 -0500

    Update to 1.3 of Stardew Valley

--- QuickStart/ModEntry.cs
+++ QuickStart/ModEntry.cs
@@ -2,13 +2,8 @@
 {
 	using System;
 	using System.Collections.Generic;
-	using System.Linq;
-	using System.Text;
-	using System.Threading.Tasks;
-	using Microsoft.Xna.Framework;
 	using StardewModdingAPI;
 	using StardewModdingAPI.Events;
-	using StardewModdingAPI.Utilities;
 	using StardewValley;
 
 	/// <summary>
